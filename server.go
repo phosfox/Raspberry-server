79c68e203162189933bb6876b28189dcb5988c71
phosfox <mordredur@googlemail.com>
2019-10-24 12:51:00 +0200

    adds array of ips

--- server.go
+++ server.go
@@ -1,31 +1,40 @@
 package main
 
-	
-
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
+type server struct {
+	ips []string
+}
 
-func ipHandle(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "%+v\n", req)
-	if err := req.ParseForm(); err != nil {
-		fmt.Fprintf(w, "Parseform() err: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "Post: %v\n", req.PostForm)
-	fmt.Fprintf(w, "IP: %s\n", req.FormValue("ip"))
-	
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
+func (s *server) ipHandle(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "POST":
+		if err := req.ParseForm(); err != nil {
+			fmt.Fprintf(w, "Parseform() err: %v", err)
+			return
 		}
+
+		ip := req.FormValue("ip")
+		s.ips = append(s.ips, ip)
+		
+		fmt.Fprintf(w, "Post: %v\n", req.PostForm)
+		fmt.Fprintf(w, "IP: %s\n", ip)
+		fmt.Fprintf(w, "IPs: %v\n", s.ips)
+	default:
+		fmt.Fprintf(w, "Not implemented yet\n")
 	}
 }
 
-func main() {
-	http.HandleFunc("/ip", ipHandle)
+func (s *server) start(){
+	http.HandleFunc("/ip", s.ipHandle)
 
 	http.ListenAndServe(":8090", nil)
+}
+
+func main() {
+	var s server
+	s.start()
 }
