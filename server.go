0fb2ea5f2df1432d82c76c02b15a6f5a7860d7cb
phosfox <mordredur@googlemail.com>
2019-10-26 00:08:26 +0200

    adds root handle to serve ips

--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -12,6 +14,14 @@ import (
 type server struct {
 	db *sql.DB
 }
+
+type dbrow struct {
+	id        int    `json:"id"`
+	ip        string `json:"ip"`
+	timestamp string `json:"timestamp"`
+}
+
+type dbrows []dbrow
 
 func (s *server) ipHandle(w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
@@ -31,7 +41,37 @@ func (s *server) ipHandle(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (s *server) rootHandle(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		rows := SelectAllIps(s.db)
+		ips := dbrows{dbrow{99, "255.255.255.255", "24:24"}}
+
+		for rows.Next() {
+			var (
+				id        int
+				ip        string
+				timestamp time.Time
+			)
+			if err := rows.Scan(&id, &ip, &timestamp); err != nil {
+				log.Fatal(err)
+			}
+			ips = append(ips, dbrow{id, ip, timestamp.Local().String()})
+		}
+		mIps, err := json.Marshal(ips)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.Write(mIps)
+	default:
+		fmt.Fprintf(w, "ROOT Default\n")
+	}
+}
+
 func (s *server) start() {
+	http.HandleFunc("/", s.rootHandle)
 	http.HandleFunc("/ip", s.ipHandle)
 
 	http.ListenAndServe(":8090", nil)
@@ -41,16 +81,5 @@ func main() {
 	var s server
 	db := GetDB()
 	s.db = db
-
-	rows := SelectAllIps(db)
-	var id int
-	var ip string
-	var timestamp time.Time
-
-	for rows.Next() {
-		rows.Scan(&id, &ip, &timestamp)
-		fmt.Printf("Id:%d IP:%s Timestamp:%s\n", id, ip, timestamp.Local())
-	}
-
 	s.start()
 }
